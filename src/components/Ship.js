3f459bff231e90bce12fddc1a3946aef88dcbd6d
Martynas Petruška <petruskamartynas@gmail.com>
2018-06-11 10:05:37 +0300

    išvalymas

--- src/components/Ship.js
+++ src/components/Ship.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import './App.css';
 
-class App extends Component {
+class Ships extends Component {
   render() {
     return (
-      <p>Hi!</p>
+      
     );
   }
 }
